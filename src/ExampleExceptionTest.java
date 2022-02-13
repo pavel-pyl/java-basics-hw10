37b98798c7f40861d611324d8b226692fe99c309
pavel-pyl <4469540@gmail.com>
2022-02-13 16:48:39 +0200

    Home work 10

--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -1,8 +1,8 @@
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.fail;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+import static org.testng.Assert.assertTrue;
 
 public class ExampleExceptionTest {
 
@@ -14,9 +14,11 @@ public class ExampleExceptionTest {
   @DataProvider(name = "data")
   public static Object[][] data() {
     return new Object[][]{
-        {2, 2, 4},
-        {2, 3, 6}
-        // TODO add 2 more test data here
+            {2, 2, 4},
+            {2, 3, 6},
+            {1, 0, 0},
+            {0, 4, 0},
+            {0b0010, 4, 8}
     };
   }
 
@@ -28,20 +30,26 @@ public class ExampleExceptionTest {
   @DataProvider(name = "negativeData")
   public static Object[][] negativeData() {
     return new Object[][]{
-        {-2, 2},
-        {2, -2}
-        // TODO add 2 more test data here
+            {-2, 2},
+            {2, -2},
+            {-2, -2},
+            //{2, 2} // for test correctness test-cases
     };
   }
 
   @Test(dataProvider = "data")
   public void testRectangleArea(int a, int b, int c) {
-    // TODO put your code here
+    assertEquals(ExampleException.rectangleArea(a, b), c, "Square is incorrect");
   }
 
 
   @Test(dataProvider = "negativeData")
   public void testRectangleAreaNegative(int a, int b) {
-    // TODO put your code here
+    try {
+      ExampleException.rectangleArea(a, b);
+      fail("This code shouldn't be executed"); // check if exception really was present.
+    } catch (IllegalArgumentException e) {
+      assertEquals(e.getMessage(), "input value is below zero!", "Message of IllegalArgumentException is incorrect");
+    }
   }
 }
